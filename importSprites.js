a34ace737e642fda715a4bb8cf5345c01c2c961b
Byson94 <Byson94@outlook.com>
2024-08-27 17:37:54 +0530

    Delete importSprites.js

--- importSprites.js
+++
@@ -1,5 +0,0 @@
-console.log('This is a testing of new feature! In this feature it should allow the player to import a pre-built sprite to the engine!')
-
-function exportButton() {
-  // Work here!
-}
