2327a442181cc36d0d010074ed57bd7e482ab881
Byson94 <Byson94@outlook.com>
2024-08-27 17:10:49 +0530

    Update and rename app/console.js to importSprites.js

--- app/console.js
+++
@@ -1,5 +0,0 @@
-console.log('This is a testing of new feature! In this feature it should allow the player to export a pre-built sprite to the engine!')
-
-function exportButton() {
-  // Work here!
-}
---
+++ importSprites.js
@@ -0,0 +1,5 @@
+console.log('This is a testing of new feature! In this feature it should allow the player to import a pre-built sprite to the engine!')
+
+function exportButton() {
+  // Work here!
+}
